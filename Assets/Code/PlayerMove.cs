f12065e3b544eaaaaa6a83b95b504792e4308f65
bhecra <ingchristianburbano@gmail.com>
2018-07-11 14:04:07 -0500

    Initial commit

---
+++ Assets/Code/PlayerMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.AddComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.position += transform.forward * Time.deltaTime;
+		}
+
+	}
+}
